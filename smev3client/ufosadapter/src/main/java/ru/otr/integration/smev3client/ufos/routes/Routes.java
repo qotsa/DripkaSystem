03b421d213f22fc0a9702319173653526b826cba
Maxim Nikitin <maxim.nikitin.work@gmail.com>
2016-08-30 13:10:12 +0300

    EB-56234 bitronix

--- smev3client/ufosadapter/src/main/java/ru/otr/integration/smev3client/ufos/routes/Routes.java
+++ smev3client/ufosadapter/src/main/java/ru/otr/integration/smev3client/ufos/routes/Routes.java
@@ -1,6 +1,5 @@
 package ru.otr.integration.smev3client.ufos.routes;
 
-import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.spring.SpringRouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -12,15 +11,15 @@ public class Routes extends SpringRouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("activemq:queue:ufosAdapterInboundQueue?transacted=true")
+        from("activemq:queue:ufosAdapterInboundQueue")
             .routeId("toUfos")
-            .transacted()
+            .transacted("PROPAGATION_REQUIRED")
             .to("log:ru.otr.integration.smev3client.ufosadapter?level=DEBUG&showAll=true&multiline=true")
             .to("activemq:queue:ufosAdapterOutboundQueue");
 
         from("scheduler://foo?initialDelay=10s&delay=10s")
-                .routeId("test")
-                .transacted()
-                .to("activemq:queue:ufosAdapterInboundQueue");
+            .routeId("test")
+            .transacted("PROPAGATION_REQUIRES_NEW")
+            .to("activemq:queue:ufosAdapterInboundQueue");
     }
 }

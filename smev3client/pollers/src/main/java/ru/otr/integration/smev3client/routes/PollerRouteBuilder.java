76f9d250c2a3bf04bb85ccc16c7ebfaedff575c9
Maxim Nikitin <maxim.nikitin.work@gmail.com>
2016-08-31 15:09:40 +0300

    EB-56234 env.properties

--- smev3client/pollers/src/main/java/ru/otr/integration/smev3client/routes/PollerRouteBuilder.java
+++ smev3client/pollers/src/main/java/ru/otr/integration/smev3client/routes/PollerRouteBuilder.java
@@ -23,34 +23,31 @@ public class PollerRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+        Namespaces ns2 = new Namespaces("ns2", "http://otr.ru/irs/services/message-exchange/types");
 
-            Namespaces ns2 = new Namespaces("ns2", "http://otr.ru/irs/services/message-exchange/types");
+        camelContext.setStreamCaching(true);
 
-
-
-            camelContext.setStreamCaching(true);
         from("scheduler://foo?initialDelay=70s&delay=70s").routeId("GetRequestPoller")
                 .transacted()
                 .to("velocity:velocity/GetRequestRequest.vm")
-                .to("http://smev3adapter:{{smev3adapter.host.port}}/camel/request")
+                .to("http://smev3adapter:{{smev3adapter.http.port}}/camel/request")
                 .to("activemq:GetRequestResponseQueue");
 
-  /*      from("scheduler://foo1?initialDelay=120s&delay=60s").routeId("GetResponsePoller")
+        /*from("scheduler://foo1?initialDelay=120s&delay=60s").routeId("GetResponsePoller")
                 .to("velocity:velocity/GetResponseRequest.vm")
                 .to("http://smev3adapter:8090/camel/request")
-                .to("activemq:GetResponseResponseQueue");
-*/
+                .to("activemq:GetResponseResponseQueue");*/
+
         from("scheduler://foo2?initialDelay=60s&delay=15s").routeId("GetStatusPoller")
                 .transacted()
                 .to("velocity:velocity/GetStatusRequest.vm")
-                .to("http://smev3adapter:{{smev3adapter.host.port}}/camel/request")
+                .to("http://smev3adapter:{{smev3adapter.http.port}}/camel/request")
                 .idempotentConsumer(xpath("//ns2:OriginalMessageId").resultType(String.class).namespaces(ns2), repository)
                 .skipDuplicate(false)
                 .filter(exchangeProperty(Exchange.DUPLICATE_MESSAGE).isEqualTo(true))
-                    .to("activemq:DuplicatesQueue")
-                    .stop()
-                    .end()
+                .to("activemq:DuplicatesQueue")
+                .stop()
+                .end()
                 .to("activemq:GetStatusResponseQueue");
-
     }
 }

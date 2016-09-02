f010f986242bc29964bfb8003499e74a2e39dcb1
Maxim Nikitin <maxim.nikitin.work@gmail.com>
2016-09-02 15:49:41 +0300

    hard-code forever

--- smev3client/replication/src/main/java/ru/otr/integration/smev3client/routes/ReplicationRouteBuilder.java
+++ smev3client/replication/src/main/java/ru/otr/integration/smev3client/routes/ReplicationRouteBuilder.java
@@ -1,24 +1,26 @@
 package ru.otr.integration.smev3client.routes;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
 /**
  * Created by tartanov.mikhail on 31.08.2016.
  */
+@Component
 public class ReplicationRouteBuilder extends RouteBuilder {
+
     @Override
     public void configure() throws Exception {
-
         from("{{routes.replicationService.inboundQueue}}").routeId("replicationService")
                 .transacted()
-                .setHeader("messageReplicationAndVerification").method(ResponseRandomizer.class, "getOkorFailed")
+                .setHeader("messageReplicationAndVerification").simple("OK")//.method(ResponseRandomizer.class, "getOkorFailed")
                 .to("{{routes.replicationService.outboundQueue}}");
     }
 
     private static class ResponseRandomizer {
-        private String getOkorFailed()  {
+        public static String getOkorFailed()  {
             return new Random().nextInt() % 2 == 0 ? "OK" : "FAILED";
         }
     }

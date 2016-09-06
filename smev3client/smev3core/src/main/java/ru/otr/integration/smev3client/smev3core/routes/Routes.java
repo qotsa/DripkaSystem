5bced0dc8ac91fcdf58224b1804ce94181f93a6e
Maxim Nikitin <maxim.nikitin.work@gmail.com>
2016-09-06 18:12:33 +0300

    EB-56442 configuration for unit & integration testing + code coverage reports

--- smev3client/smev3core/src/main/java/ru/otr/integration/smev3client/smev3core/routes/Routes.java
+++ smev3client/smev3core/src/main/java/ru/otr/integration/smev3client/smev3core/routes/Routes.java
@@ -12,21 +12,21 @@ public class Routes extends RouteBuilder {
         Namespaces ns = new Namespaces("bas", "http://otr.ru/irs/services/message-exchange/types/basic")
                 .add("typ2", "http://otr.ru/irs/services/message-exchange/types");
 
-        from("{{routes.Smev2Vis.preprocessor.GetRequestResponseQueue}}").to("direct:Smev2Vis.preprocessor");
-        from("{{routes.Smev2Vis.preprocessor.GetResponseResponseQueue}}").to("direct:Smev2Vis.preprocessor");
-        from("direct:Smev2Vis.preprocessor").to("{{routes.Smev2Vis.preprocessor.inbound}}");
+        from("{{routes.Smev2Vis.preprocessor.GetRequestResponseQueue}}").to("direct:Smev2Vis_preprocessor");
+        from("{{routes.Smev2Vis.preprocessor.GetResponseResponseQueue}}").to("direct:Smev2Vis_preprocessor");
+        from("direct:Smev2Vis_preprocessor").to("{{routes.Smev2Vis.preprocessor.inbound}}");
 
         from("{{routes.Smev2Vis.preprocessor.inbound}}")
                 .transacted()
                 .routeId("Smev2VisPreprocessor")
                 .choice()
                     .when(ns.xpath("//bas:FSAttachmentsList/bas:FSAttachment"))
-                        .to("{{routes.Smev2Vis.replication}}").stop()
+                        .to("{{routes.replication}}").stop()
                     .otherwise()
-                        .to("direct:Smev2Vis.postprocessor")
+                        .to("direct:Smev2Vis_postprocessor")
                 .end();
 
-        from("direct:Smev2Vis.postprocessor").to("{{routes.Smev2Vis.postprocessor.inbound}}");
+        from("direct:Smev2Vis_postprocessor").to("{{routes.Smev2Vis.postprocessor.inbound}}");
 
         from("{{routes.Smev2Vis.postprocessor.inbound}}")
                 .transacted()
@@ -39,21 +39,21 @@ public class Routes extends RouteBuilder {
                         .dynamicRouter(method(PostprocessorRouter.class, "route"))
                 .end();
 
-        from("{{routes.Vis2Smev.preprocessor.SendRequestResponseQueue}}").to("direct:Vis2Smev.preprocessor");
-        from("{{routes.Vis2Smev.preprocessor.SendResponseResponseQueue}}").to("direct:Vis2Smev.preprocessor");
-        from("direct:Vis2Smev.preprocessor").to("{{routes.Vis2Smev.preprocessor.inbound}}");
+        from("{{routes.Vis2Smev.preprocessor.SendRequestResponseQueue}}").to("direct:Vis2Smev_preprocessor");
+        from("{{routes.Vis2Smev.preprocessor.SendResponseResponseQueue}}").to("direct:Vis2Smev_preprocessor");
+        from("direct:Vis2Smev_preprocessor").to("{{routes.Vis2Smev.preprocessor.inbound}}");
 
         from("{{routes.Vis2Smev.preprocessor.inbound}}")
                 .transacted()
                 .routeId("Vis2SmevPreprocessor")
                 .choice()
                 .when(ns.xpath("//bas:FSAttachmentsList/bas:FSAttachment"))
-                .to("{{routes.Vis2Smev.replication}}").stop()
+                .to("{{routes.replication}}").stop()
                 .otherwise()
-                .to("direct:Vis2Smev.postprocessor")
+                .to("direct:Vis2Smev_postprocessor")
                 .end();
 
-        from("direct:Vis2Smev.postprocessor").to("{{routes.Vis2Smev.postprocessor.inbound}}");
+        from("direct:Vis2Smev_postprocessor").to("{{routes.Vis2Smev.postprocessor.inbound}}");
 
         // TODO route to pushers module
 

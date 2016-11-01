62842e49d1e0d6215fd11dfd2299124b810478fb
Maxim Nikitin <maxim.nikitin.work@gmail.com>
2016-11-01 17:41:24 +0300

    EB-57535 small refactoring

--- smev3client/sh/smev3client-services.sh
+++ smev3client/sh/smev3client-services.sh
@@ -22,9 +22,6 @@ IMAGES_VERSION=1.0
 SERVICE_PARAMS_COMMON="--network smev3client --replicas 1 --log-opt max-size=10m --log-opt max-file=10 --with-registry-auth --restart-condition none"
 SERVICE_CONSTRAINTS="--constraint node.labels.nodeType==worker"
 
-#SERVICES=("eureka" "configserver" "smev3core" "smev3adapter" "ufosadapter" "replication" "pollers" "pushers" "smev3mock2" "bpmengine")
-SERVICES=("eureka" "configserver" "smev3core" "smev3adapter" "ufosadapter" "replication" "pushers" "smev3mock2" "bpmengine")
-
 declare -A SERVICES_INFRA
 SERVICES_INFRA[activemq]="-p 8161:8161 -p 61616:61616 -p 61613:61613 --env \"ACTIVEMQ_MIN_MEMORY=512\" --env \"ACTIVEMQ_MAX_MEMORY=2048\" --env \"ACTIVEMQ_NAME=amqp-srv1\" --env \"ACTIVEMQ_REMOVE_DEFAULT_ACCOUNT=true\" --env \"ACTIVEMQ_ADMIN_LOGIN=admin\" --env \"ACTIVEMQ_ADMIN_PASSWORD=admin\" --env \"ACTIVEMQ_WRITE_LOGIN=producer\" --env \"ACTIVEMQ_WRITE_PASSWORD=producer\" --env \"ACTIVEMQ_READ_LOGIN=consumer\" --env \"ACTIVEMQ_READ_PASSWORD=consumer\" --env \"ACTIVEMQ_JMX_LOGIN=jmx\" --env \"ACTIVEMQ_JMX_PASSWORD=jmx\" --env \"ACTIVEMQ_ENABLED_SCHEDULER=true\""
 SERVICES_INFRA[ftp]="-p 2021:2021 -p 30000-30009:30000-30009 --mount type=volume,source=ftpData,destination=/home/ftpusers --mount type=volume,source=ftpUsers,destination=/etc/pure-ftpd/users --env \"PUBLICHOST=ftp\""
@@ -36,6 +33,18 @@ SERVICES_INFRA[curator]=""
 SERVICES_INFRA[cadvisor]="-p 8081:8081 --mount type=bind,source=/../,destination=/rootfs:ro --mount type=bind,source=/var/run,destination=/var/run:rw --mount type=bind,source=/sys,destination=/sys:ro --mount type=bind,source=/var/lib/docker/,destination=/var/lib/docker:ro"
 SERVICES_INFRA[kibana]="-p 5601:5601 --env \"--max-old-space-size=250\""
 SERVICES_INFRA[grafana]="-p 3000:3000"
+
+declare -A SERVICES
+SERVICES_INFRA[eureka]="-p 8097:8097"
+SERVICES_INFRA[configserver]="-p 8888:8888"
+SERVICES_INFRA[smev3core]="-p 8093:8093"
+SERVICES_INFRA[smev3adapter]="-p 8090:8090"
+SERVICES_INFRA[ufosadapter]="-p 8094:8094"
+SERVICES_INFRA[replication]="-p 8095:8095"
+#SERVICES_INFRA[pollers]="-p 8092:8092 -p 5701:5701"
+SERVICES_INFRA[pushers]="-p 8096:8096"
+SERVICES_INFRA[smev3mock2]="-p 8091:8091"
+SERVICES_INFRA[bpmengine]="-p 8098:8098"
 
 VOLUMES=("ftpData" "ftpUsers" "ftpSmevData" "ftpSmevUsers" "postgresData" "elasticsearchData")
 
@@ -78,29 +87,18 @@ case $operation in
         log1 "END of infrastructure services section"
 
         sleep 1
-        log1 "Spring cloud infrastructure services section"
+        log1 "Business services section"
         echoProgress
 
-        #docker service create ${SERVICE_CONSTRAINTS} ${SERVICE_PARAMS_COMMON} --name eureka       -p 8097:8097 ${REGISTRY}/eureka:${IMAGES_VERSION}
-        #docker service create ${SERVICE_CONSTRAINTS} ${SERVICE_PARAMS_COMMON} --name configserver -p 8888:8888 ${REGISTRY}/configserver:${IMAGES_VERSION}
-        #docker service create ${SERVICE_CONSTRAINTS} ${SERVICE_PARAMS_COMMON} --name smev3core    -p 8093:8093 ${REGISTRY}/smev3core:${IMAGES_VERSION}
-        #docker service create ${SERVICE_CONSTRAINTS} ${SERVICE_PARAMS_COMMON} --name smev3adapter -p 8090:8090 ${REGISTRY}/smev3adapter:${IMAGES_VERSION}
-        #docker service create ${SERVICE_CONSTRAINTS} ${SERVICE_PARAMS_COMMON} --name ufosadapter  -p 8094:8094 ${REGISTRY}/ufosadapter:${IMAGES_VERSION}
-        #docker service create ${SERVICE_CONSTRAINTS} ${SERVICE_PARAMS_COMMON} --name replication  -p 8095:8095 ${REGISTRY}/replication:${IMAGES_VERSION}
-        #docker service create ${SERVICE_CONSTRAINTS} ${SERVICE_PARAMS_COMMON} --name pollers      -p 8092:8092 -p 5701:5701 ${REGISTRY}/pollers:${IMAGES_VERSION}
-        #docker service create ${SERVICE_CONSTRAINTS} ${SERVICE_PARAMS_COMMON} --name pushers      -p 8096:8096 ${REGISTRY}/pushers:${IMAGES_VERSION}
-        #docker service create ${SERVICE_CONSTRAINTS} ${SERVICE_PARAMS_COMMON} --name smev3mock2   -p 8091:8091 ${REGISTRY}/smev3mock2:${IMAGES_VERSION}
-        #docker service create ${SERVICE_CONSTRAINTS} ${SERVICE_PARAMS_COMMON} --name bpmengine    -p 8098:8098 ${REGISTRY}/bpmengine:${IMAGES_VERSION}
+        for service in "${!SERVICES[@]}"
+        do
+           echo docker service create ${SERVICE_CONSTRAINTS} ${SERVICE_PARAMS_COMMON} --name ${service} ${SERVICES[${service}]} ${REGISTRY}/${service}:${IMAGES_VERSION}
+        done
 
         echoProgress
-        log1 "Existing networks:"
-        docker network ls
 
-        log1 "Existing services:"
-        docker service ls
+        ./swarm-info.sh
 
-        log1 "Existing volumes:"
-        docker volume ls
         sleep 1
         log1 "THE END"
         ;;
@@ -113,10 +111,10 @@ case $operation in
            docker pull ${REGISTRY}/${service}:${IMAGES_VERSION}
         done
 
-        #for service in "${SERVICES[@]}"
-        #do
-        #   docker pull ${REGISTRY}/${service}:${IMAGES_VERSION}
-        #done
+        for service in "${!SERVICES[@]}"
+        do
+           docker pull ${REGISTRY}/${service}:${IMAGES_VERSION}
+        done
         ;;
 
     scale_down)
@@ -127,10 +125,10 @@ case $operation in
            docker service scale ${service}=0
         done
 
-        #for service in "${SERVICES[@]}"
-        #do
-        #   docker service scale ${service}=0
-        #done
+        for service in "${!SERVICES[@]}"
+        do
+           docker service scale ${service}=0
+        done
         ;;
 
     scale_up)
@@ -141,10 +139,10 @@ case $operation in
            docker service scale ${service}=1
         done
 
-        #for service in "${SERVICES[@]}"
-        #do
-        #   docker service scale ${service}=1
-        #done
+        for service in "${!SERVICES[@]}"
+        do
+           docker service scale ${service}=1
+        done
         ;;
 
     *)
